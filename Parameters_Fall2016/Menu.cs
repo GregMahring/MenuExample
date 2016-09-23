faedd247410d0a7208851dbf96d6e370c38701d4
Jarryd Huntley <jarryd.huntley@gmail.com>
2016-09-23 09:49:09 -0400

    initial commit.

---
+++ Parameters_Fall2016/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Fall2016
+{
+    class Menu
+    {
+        //fields
+
+        //properties
+
+        //constructors
+
+        //methods
+
+
+    }
+}
